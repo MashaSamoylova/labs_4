e77d4ed04a2027de12b8ffc531e8e3e0a026db4f
Masha Samoylova <mashanedyak@gmail.com>
2017-05-21 00:14:07 +0700

    6 symbols is decoded

--- kodir/arithmetic_coding/arithmetic.c
+++ kodir/arithmetic_coding/arithmetic.c
@@ -44,6 +44,92 @@ int count_frequency(char *s, symbol_with_freq b[]) {
 	return 0;
 }
 
+int to_int(char *s) {
+	int i;
+	int value = 0;
+	for(i = 0; i < 16; i++) {
+		if('\0' == s[i]) {
+			return -1;
+		}
+		if('1' == s[i]) {
+			value = value | (1 << (15-i));
+		}	
+	}
+	return value;
+}
+
+int input_bit(char* s) {
+	if('1' == *s) {
+		return 1;
+	}
+	if('0' == *s) {
+		return 0;
+	}
+	printf("end of the code\n");
+	exit(0);
+}
+
+void decode(char* code, symbol_with_freq b[]) {
+	int value;
+	int l[strlen(code)]; //исходный рабочий интервал
+	int h[strlen(code)];
+	h[0] = 65535;
+	l[0] = 0;
+	int first_qtr = h[0]/4+1;
+	int half = 2*first_qtr;
+	int third_qtr = 3*first_qtr;
+	int range;
+	int index = 0;
+	int i = 1;
+	int cum = 0;
+	int c_index;
+	int counter = 17;
+	value = to_int(code+index);
+	index = 16;
+
+	printf("\n\nDECODING:\n");
+	while(counter > 0) {
+		if(-1 == value) {
+			break;
+		}
+		printf("value = %d\n", value);
+		range = (h[i-1] - l[i-1]) + 1;
+		cum = ((value - l[i-1] + 1) * DEL - 1) / range;
+		printf("cum = %d\n", cum);
+		printf("b[c_index].symbol: %c, frequency: %d\n", b[1].symbol, b[1].frequency);
+		for(c_index = 1; b[c_index].frequency < cum; c_index++){
+		}
+		printf("decode symbol: %c\n", b[c_index].symbol);
+		printf("[%d;%d]\n", l[i], h[i]);
+		l[i] = l[i-1] + (range*b[c_index-1].frequency)/DEL-1;
+		h[i] = l[i-1] + (range*b[c_index].frequency)/DEL;
+		printf("[%d;%d]\n", l[i], h[i]);
+		for(;;) {
+			if(h[i] >= half) {
+				if(l[i] >= half) {
+					value -= half; 
+					l[i] -= half;
+					h[i] -= half;
+				}
+				else if(l[i] >= first_qtr && h[i] < third_qtr) {
+					value -= first_qtr;
+					l[i] -= first_qtr;
+					h[i] -= first_qtr;
+				}
+				else break;
+			}
+			l[i] = 2*l[i];
+			h[i] = 2*h[i] + 1;
+			value = 2*value + input_bit(code+index);
+			index++;
+		}
+		printf("------------------\n");
+	printf("\n%d\n", index);
+	i++;
+	counter--;
+	}
+}
+
 int search(symbol_with_freq b[], char c) {
 	int i = 0;
 	while(b[i].symbol != c) {
@@ -132,48 +218,21 @@ int encode(char *s, symbol_with_freq b[]) {
 	}
 
 	printf("%s", code);
+	decode(code, b);
 	return 0;
 }
 
+int main(int argc, char* argv[]) { 
 
-int main(int argc, char* argv[]) {
-	if(argc < 3) {
-		printf("please, use ./haffman <filename> <filename>\n");
-		return 0;
-	}
-
-	FILE *fd;
-	fd = fopen(argv[1], "r");
-	if(fd == NULL) {
-		printf("SOME ERROR:(\n");
-		return 0;
-	}
-
-	char* s = malloc( sizeof(char)*1000 );
-	
-	int i = 0;
-	do {
-		s[i] = fgetc(fd);
-		if(s[i] == EOF) {
-			if(feof(fd)) {
-				printf("Считывание завершено\n");
-				break;
-			}
-		}
-		i++;
-	} while(1);
-	s[i] = '\0';
-	
+	char s[50];
+	printf("input string: \n");	
+	scanf("%s", s);
 	printf("считанное = %s\n", s);
-	fclose(fd);
 
-	s = "baaa\0";
-	printf("s = %s\n", s);
 
 	symbol_with_freq b[strlen(s)];
 	count_frequency(s, b);
 	encode(s,b);
 
-//	free(s);
 	return 0;
 }
