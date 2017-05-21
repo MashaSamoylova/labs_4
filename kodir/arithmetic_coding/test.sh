002e5cbc21fe6fb27c796364a4a3c98ff8b03cf7
Masha Samoylova <mashanedyak@gmail.com>
2017-05-21 14:58:45 +0700

    i am ready

--- kodir/arithmetic_coding/test.sh
+++ kodir/arithmetic_coding/test.sh
@@ -5,9 +5,8 @@ LIMIT=1000
 
 while [ "$var0" -lt "$LIMIT" ]
 do
-#  echo  "$var0 "        # -n подавляет перевод строки.
   date +%s | sha256sum | base64 | head -c 32 | ./arithmetic > lol; diff lol check
-  var0=`expr $var0 + 1`   # допускается var0=$(($var0+1)).
+  var0=`expr $var0 + 1`  
 done
 
 echo
